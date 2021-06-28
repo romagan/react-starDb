c6a331e20cf0b3456a4ae207b1ef214b0aaf0ad6
Roman Konashenkov <konashenkov@obit.ru>
2021-06-28 16:23:17 +0300

    Updates

---
+++ precache-manifest.7bc8c0d0ffd694cb70d8fbc48c3efcdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4390ab8caa850d8cdcfd20e0b2d418",
+    "url": "/react-stardb/index.html"
+  },
+  {
+    "revision": "05a7da87daad5b40db71",
+    "url": "/react-stardb/static/css/main.79ea1354.chunk.css"
+  },
+  {
+    "revision": "19223337ae1be8447754",
+    "url": "/react-stardb/static/js/2.99f99e8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-stardb/static/js/2.99f99e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a7da87daad5b40db71",
+    "url": "/react-stardb/static/js/main.5fd5bcdf.chunk.js"
+  },
+  {
+    "revision": "05ca1ad50a3ba05613a0",
+    "url": "/react-stardb/static/js/runtime-main.5e4613bc.js"
+  }
+]);
